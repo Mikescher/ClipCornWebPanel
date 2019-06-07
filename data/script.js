2d663c136a5765ee0d30cf352c5c9e7aed374f91
Mike Schwörer <mailport@mikescher.de>
2019-06-07 19:40:53 +0200

    sidebar

--- data/script.js
+++ data/script.js
@@ -7,16 +7,107 @@ $(window).on('load', function()
 		{
 			let json = JSON.parse(data);
 
+			let groups = [];
+			let genres = [];
+			let langs  = [];
+
 			for (let e of json)
 			{
 				if (e['ser']) addSeriesEntry(e);
 				else          addMovieEntry(e);
+
+				for (let vvv of e['grp']) if (!groups.includes(vvv)) groups.push(vvv);
+				for (let vvv of e['gnr']) if (!genres.includes(vvv)) genres.push(vvv);
+				for (let vvv of e['lng']) if (!langs.includes(vvv))  langs.push(vvv);
 			}
+
+			$('#rippleloader').remove();
+
+			setSidebarValues(false, groups, true, $('#sc_1'), null, null);
+			setSidebarValues(false, genres, false, $('#sc_2'), null, getGenreTitle);
+			setSidebarValues(false, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], false, $('#sc_3'), 'onlinescore-', null);
+			setSidebarValues(false, [0, 1, 2, 3, 4, 5, 6], false, $('#sc_4'), 'score-', getScoreTitle);
+			setSidebarValues(false, [0, 1, 2, 3, 4], false, $('#sc_5'), 'fsk-', getFSKTitle);
+			setSidebarValues(false, [0, 1, 2, 3, 4, 5, 6, 7, 8], false, $('#sc_6'), 'format-', getFormatTitle);
+			setSidebarValues(false, [0, 1, 2, 3, 4], false, $('#sc_7'), 'fsk-', getQualityTitle);
+			setSidebarValues(false, [0, 1, 2, 3, 4, 5, 6, 7, 8], false, $('#sc_8'), 'tag-', getTagTitle);
+			setSidebarValues(true,  langs, false, $('#sc_9'), 'lang-', getLangTitle);
+			setSidebarValues(false, [0, 1], false, $('#sc_A'), 'typ-', function (i) {return (i===0)?'Movie':'Series'});
+			setSidebarValues(false, [0, 1], false, $('#sc_B'), 'view-', function (i) {return (i===1)?'Viewed':'Not viewed'});
 
 			return data;
 		}
 	});
+
+	$("#sb_0").click(function () { collapseSidebar(0x0); });
+	$("#sb_1").click(function () { collapseSidebar(0x1); });
+	$("#sb_2").click(function () { collapseSidebar(0x2); });
+	$("#sb_3").click(function () { collapseSidebar(0x3); });
+	$("#sb_4").click(function () { collapseSidebar(0x4); });
+	$("#sb_5").click(function () { collapseSidebar(0x5); });
+	$("#sb_6").click(function () { collapseSidebar(0x6); });
+	$("#sb_7").click(function () { collapseSidebar(0x7); });
+	$("#sb_8").click(function () { collapseSidebar(0x8); });
+	$("#sb_9").click(function () { collapseSidebar(0x9); });
+	$("#sb_A").click(function () { collapseSidebar(0xA); });
+	$("#sb_B").click(function () { collapseSidebar(0xB); });
+
+	collapseSidebar(0);
 });
+
+function collapseSidebar(v) {
+	if (v !== 0x0) $("#sc_0").css('visibility', 'collapse'); else $("#sc_0").css('visibility', 'visible');
+	if (v !== 0x1) $("#sc_1").css('visibility', 'collapse'); else $("#sc_1").css('visibility', 'visible');
+	if (v !== 0x2) $("#sc_2").css('visibility', 'collapse'); else $("#sc_2").css('visibility', 'visible');
+	if (v !== 0x3) $("#sc_3").css('visibility', 'collapse'); else $("#sc_3").css('visibility', 'visible');
+	if (v !== 0x4) $("#sc_4").css('visibility', 'collapse'); else $("#sc_4").css('visibility', 'visible');
+	if (v !== 0x5) $("#sc_5").css('visibility', 'collapse'); else $("#sc_5").css('visibility', 'visible');
+	if (v !== 0x6) $("#sc_6").css('visibility', 'collapse'); else $("#sc_6").css('visibility', 'visible');
+	if (v !== 0x7) $("#sc_7").css('visibility', 'collapse'); else $("#sc_7").css('visibility', 'visible');
+	if (v !== 0x8) $("#sc_8").css('visibility', 'collapse'); else $("#sc_8").css('visibility', 'visible');
+	if (v !== 0x9) $("#sc_9").css('visibility', 'collapse'); else $("#sc_9").css('visibility', 'visible');
+	if (v !== 0xA) $("#sc_A").css('visibility', 'collapse'); else $("#sc_A").css('visibility', 'visible');
+	if (v !== 0xB) $("#sc_B").css('visibility', 'collapse'); else $("#sc_B").css('visibility', 'visible');
+}
+
+
+function setSidebarValues(presorted, values, sorted, target, icon, txtconvert)
+{
+	if (presorted) values.sort();
+
+	if (txtconvert !== null)
+	{
+		let values2 = [];
+		for(let val of values)
+		{
+			let txt = txtconvert(val);
+			let icn = '';
+			if (icon !== null) icn = '<i class="icn ' + icon + val + '"></i>';
+			values2.push(icn + txt);
+		}
+		values = values2;
+	}
+	else if (icon !== null)
+	{
+		let values2 = [];
+		for(let val of values)
+		{
+			values2.push('<i class="icn ' + icon + val + '"></i>');
+		}
+		values = values2;
+	}
+
+	if (sorted) values.sort();
+
+	let html = '';
+
+	for(let val of values)
+	{
+		html += '<a href="#" class="sidebar-level-2">'+val+'</a>';
+	}
+
+	target.append(html)
+}
 
 function fmtZyklusNum(num)
 {
@@ -30,33 +121,40 @@ function fmtZyklusNum(num)
 	return ' ' + Array(+digits.join("") + 1).join("M") + roman;
 }
 
-function getScoreTitle(scr) {
-	return ['Crappy Movie', 'Bad Movie', 'Not recommended', 'Good enough', 'Recommended', 'Masterpiece', ''][scr];
+function getScoreTitle(scr)
+{
+	return ['Crappy Movie', 'Bad Movie', 'Not recommended', 'Good enough', 'Recommended', 'Masterpiece', '<i>Unset</i>'][scr];
 }
 
-function getLangTitle(lng) {
+function getLangTitle(lng)
+{
 	if (lng===-1) return 'None';
 	return ['German', 'English', 'Muted', 'French', 'Japanese', 'Italian', 'Spanish', 'Portuguese', 'Danish', 'Finnish', 'Swedisch', 'Norwegian', 'Dutch', 'Czech', 'Polish', 'Turkish', 'Hungarian', 'Bulgarian', 'Russian', 'Chinese'][lng];
 }
 
-function getQualityTitle(qal) {
+function getQualityTitle(qal)
+{
 	return ['Stream', 'CD', 'CD\'s', 'DVD', 'BluRay'][qal];
 }
 
-function getFSKTitle(fsk) {
+function getFSKTitle(fsk)
+{
 	return ['FSK-0', 'FSK-6', 'FSK-12', 'FSK-16', 'FSK-18'][fsk];
 }
 
-function getFormatTitle(fmt) {
+function getFormatTitle(fmt)
+{
 	return ['MKV', 'AVI', 'MPEG', 'IMG', 'IFO', 'WMV', 'MP4', 'DIVX', 'FLV'][fmt];
 }
 
-function getTagTitle(tag) {
+function getTagTitle(tag)
+{
 	return ['Low Quality', 'Missing Time', 'Broken File', 'Watch later', 'Wrong Language', 'Watch never', 'CamRip', 'MicDubbed', 'Cancelled'][tag];
 }
 
-function getGenreTitle(tag) {
-	return ['NO_GENRE', 'Disaster Movie', 'Road Movie', 'Western', 'Italo-Western', 'Sentimental film', 'Thriller', 'Actionthriller', 'Psychothriller', 'Science Fiction', 'Comedy', 'Slapstick-Comedy', 'Screwball-Comedy', 'Gangstermovie', 'Crime Story', 'War Movie', 'Porno', 'Softporno', 'Hardcore-Porno', 'Actionmovie', 'Animation', 'Cartoon', 'Anime', 'Stop-Motion-Movie', 'Puppet Movie', 'Claymation', 'Computer Animation Mo', 'Martial-Arts-Movie', 'Samuraimovie', 'Horror Movie', 'Slasher', 'Teenhorror', 'Creature', 'ComingOfAge', 'Documentary', 'Adventure', 'Romance', 'Mystery', 'Fantasy', 'Family', 'Drama', 'Anti-War Movie', 'Biographie', 'Sport', 'Music', 'Musical', 'History', 'Film Noir', 'kids movie', 'Soap opera', 'Slice of Life', 'Shounen', 'Shoujo', 'School', 'Mecha', 'Parody', 'Isekai'][tag];
+function getGenreTitle(tag)
+{
+	return ['NO_GENRE', 'Disaster Movie', 'Road Movie', 'Western', 'Italo-Western', 'Sentimental film', 'Thriller', 'Actionthriller', 'Psychothriller', 'Science Fiction', 'Comedy', 'Slapstick-Comedy', 'Screwball-Comedy', 'Gangstermovie', 'Crime Story', 'War Movie', 'Porno', 'Softporno', 'Hardcore-Porno', 'Actionmovie', 'Animation', 'Cartoon', 'Anime', 'Stop-Motion-Movie', 'Puppet Movie', 'Claymation', 'Computer Animation Movie', 'Martial-Arts-Movie', 'Samuraimovie', 'Horror Movie', 'Slasher', 'Teenhorror', 'Creature', 'ComingOfAge', 'Documentary', 'Adventure', 'Romance', 'Mystery', 'Fantasy', 'Family', 'Drama', 'Anti-War Movie', 'Biographie', 'Sport', 'Music', 'Musical', 'History', 'Film Noir', 'Kids movie', 'Soap opera', 'Slice of Life', 'Shounen', 'Shoujo', 'School', 'Mecha', 'Parody', 'Isekai'][tag];
 }
 
 function formatLength(mins) {
@@ -170,10 +268,49 @@ function addMovieEntry(e)
 
 
 	$("#maintable").append(html);
-
 }
 
 function addSeriesEntry(e)
 {
+	let html = '';
 
+	html += '<div class="entry seriesentry">\r\n';
+	html += '<div class="coverbox">';
+	html += '<img class="coveroverlay" src="/data/mask_series.png" alt="Overlay">\r\n';
+	html += '<img class="cover" src="https://via.placeholder.com/24x33/000000/FFFFFF" alt="Cover">\r\n';
+	if (e['vwd']) html += '<i class="viewed icn viewed-1"></i>\r\n';
+	html += '</div>\r\n';
+
+	if (e['scr'] !== 6) html += '<i title="'+getScoreTitle(e['scr'])+'" class="score icn score-'+e['scr']+'"></i>\r\n';
+
+	html += '<div class="text">\r\n';
+	html += '<span class="title">' + e['name'] + '</span>\r\n';
+	html += '</div>\r\n';
+
+	html += '<div class="icons">\r\n';
+	html += '<div class="onlinescore" title="' + (e['oscr'] / 2) + ' / 10" ><i class="icn onlinescore-'+ e['oscr'] + '"></i></div>\r\n';
+	html += '<div class="quality_fsk_format"><i title="'+getFSKTitle(e['fsk'])+'" class="icn fsk-'+e['fsk']+'"></i></div>\r\n';
+	html += '<div class="tags">\r\n';
+	for (let tag of e['tgs']) html += '<i title="'+getTagTitle(tag)+'" class="icn tag-'+tag+'"></i>';
+	html += '</div>\r\n';
+	html += '</div>\r\n';
+
+	html += '<div class="info">';
+	html += '<div class="genres"><div class="cap">Genres:</div>\r\n';
+	let f1 = false;
+	for (let genre of e['gnr']) { if (f1) html+=' &#183; '; html += '<span>'+getGenreTitle(genre)+'</span>'; f1=true; }
+	html += '</div>\r\n';
+	if (e['grp'].length > 0)
+	{
+		html += '<div class="groups"><div class="cap">Groups:</div>\r\n';
+		let f2 = false;
+		for (let group of e['grp']) { if (f2) html+=' &#183; '; html += '<span>'+group+'</span>'; f2=true; }
+		html += '</div>\r\n';
+	}
+	html += '</div>\r\n';
+
+	html += '</div>\r\n';
+
+
+	$("#maintable").append(html);
 }
--- index.php
+++ index.php
@@ -5,14 +5,39 @@
 	<meta charset="utf-8">
 	<title>jClipCorn - Web panel</title>
 	<link rel="icon" type="image/png" href="/data/icon.png"/>
-	<link rel="stylesheet" href="/data/styles.css"/>
+    <link rel="stylesheet" href="/data/styles.css"/>
+    <link rel="stylesheet" href="/data/styles-icons.css"/>
+    <link rel="stylesheet" href="/data/styles-ext.css"/>
 </head>
 <body onload="script();">
 
 	<div id="root">
 
 		<div id="sidebar">
-			Sidebar
+            <a href="#" id="sb_0" class="sidebar-level-1">All</a>
+            <div id="sc_0"></div>
+            <a href="#" id="sb_1" class="sidebar-level-1">Groups</a>
+            <div id="sc_1"></div>
+            <a href="#" id="sb_2" class="sidebar-level-1">Genres</a>
+            <div id="sc_2"></div>
+            <a href="#" id="sb_3" class="sidebar-level-1">Rating</a>
+            <div id="sc_3"></div>
+            <a href="#" id="sb_4" class="sidebar-level-1">User score</a>
+            <div id="sc_4"></div>
+            <a href="#" id="sb_5" class="sidebar-level-1">Parental rating</a>
+            <div id="sc_5"></div>
+            <a href="#" id="sb_6" class="sidebar-level-1">Format</a>
+            <div id="sc_6"></div>
+            <a href="#" id="sb_7" class="sidebar-level-1">Quality</a>
+            <div id="sc_7"></div>
+            <a href="#" id="sb_8" class="sidebar-level-1">Tags</a>
+            <div id="sc_8"></div>
+            <a href="#" id="sb_9" class="sidebar-level-1">Language</a>
+            <div id="sc_9"></div>
+            <a href="#" id="sb_A" class="sidebar-level-1">Type</a>
+            <div id="sc_A"></div>
+            <a href="#" id="sb_B" class="sidebar-level-1">Viewed</a>
+            <div id="sc_B"></div>
 		</div>
 
 		<div id="content">
@@ -25,6 +50,7 @@
             <div id="maintable">
 
                 <!-- AJAX -->
+                <div id="rippleloader" class="lds-ripple"><div></div><div></div></div>
 
 			</div>
 
